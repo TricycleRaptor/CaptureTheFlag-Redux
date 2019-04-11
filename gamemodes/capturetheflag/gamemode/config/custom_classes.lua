7fab5e1e72e7fa63d4f60a835a1ffebf1d3eb5c0
Arrin Bevers <arrinbevers@yahoo.com>
2019-04-11 00:58:45 -0700

    Updated class loadouts
    * Removed the gravity gun from all classes except for the engineer. 
    * Changed the engineer primary weapon from the CW2.0 MP5 to the CW2.0 UECW G36C. 
    * Changed binoculars on the scout from the standard binoculars to the scout binoculars
    * Gave the marksman standard binoculars

--- gamemodes/capturetheflag/gamemode/config/custom_classes.lua
+++ gamemodes/capturetheflag/gamemode/config/custom_classes.lua
@@ -11,35 +11,35 @@ PLAYER_CLASSES = {
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_ar15", "cw_shorty", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
+		weapons = {"weapon_crowbar", "cw_ar15", "cw_shorty", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
 	},
 	{
 		name = "Marksman",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_m98b", "cw_mp5", "cw_fiveseven", "cw_smoke_grenade"}
+		weapons = {"weapon_crowbar", "cw_g4p_m98b", "cw_mp5", "cw_fiveseven", "weapon_rpw_binoculars" ,"cw_smoke_grenade"}
 	},
 	{
 		name = "Machinegunner",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_m249_official", "cw_fiveseven", "cw_frag_grenade"}
+		weapons = {"weapon_crowbar", "cw_m249_official", "cw_fiveseven", "cw_frag_grenade"}
 	},
 	{
 		name = "Demolitionist",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "weapon_rpg", "weapon_ied", "cw_g4p_m16a4"}
+		weapons = {"weapon_crowbar", "weapon_rpg", "weapon_ied", "cw_g4p_m16a4"}
 	},
 	{
 		name = "Arsonist",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_ump45", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan"}
+		weapons = {"weapon_crowbar", "cw_ump45", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan"}
 	},
 	{
 		name = "Engineer",
@@ -53,7 +53,7 @@ PLAYER_CLASSES = {
 		health = 100,
 		walkspeed = 300,
 		runspeed = 450,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_mp5", "cw_fiveseven", "weapon_rpw_binoculars", "parkourmod"}
+		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_g36c", "cw_fiveseven", "weapon_rpw_binoculars_scout", "parkourmod"}
 	},
 	{
 		name = "Medic",
