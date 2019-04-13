ab75f000eab98bb8674d9ad7f5ed090c34d78976
Arrin Bevers <arrinbevers@yahoo.com>
2019-04-12 19:39:01 -0700

    Added HL2 RPG Crate

---
+++ lua/entities/hl2_ammo_crate_rpg.lua
@@ -0,0 +1,45 @@
+
+AddCSLuaFile()
+
+ENT.Type 			= "anim"
+ENT.Base 			= "base_anim"
+ENT.PrintName		= "RPG Ammo Crate"
+ENT.Author			= "FiLzO, TricycleRaptor"
+ENT.Information		= ""
+ENT.Category		= "Half-Life 2"
+ENT.Cost			= 500
+ENT.Model			= "models/Items/ammoCrate_Rockets.mdl"
+
+ENT.Spawnable		= true
+ENT.AdminOnly		= false
+
+if SERVER then
+
+function ENT:Initialize()
+	
+	self:SetModel("models/weapons/shell.mdl")
+	self:SetNoDraw(true)
+	self:DrawShadow(false)
+	
+	self.ammo_crate_rpg = ents.Create("item_ammo_crate")
+	self.ammo_crate_rpg:SetPos( self:GetPos() + Vector(0,0,-48))
+	self.ammo_crate_rpg:SetAngles( self:GetAngles() )
+	self.ammo_crate_rpg:SetKeyValue( "AmmoType", 3 )
+	self.ammo_crate_rpg:SetOwner( self.Owner )
+	self.ammo_crate_rpg:Spawn()
+	self.ammo_crate_rpg:Activate()
+	local ammo_crate_rpg_name = "ammo_crate_rpg" .. self.ammo_crate_rpg:EntIndex()
+	self.ammo_crate_rpg:SetName( ammo_crate_rpg_name )
+
+end
+function ENT:Think()
+	if !IsValid (self.ammo_crate_rpg) then
+	self:Remove()
+	end
+end
+function ENT:OnRemove()
+	if IsValid (self.ammo_crate_rpg) then
+	self.ammo_crate_rpg:Remove()
+	end
+end
+end
