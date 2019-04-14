34d57182da910fbdf592731a7cf5e6f56d830d16
Arrin Bevers <arrinbevers@yahoo.com>
2019-04-13 17:19:06 -0700

    CW2.0 Attachment support

---
+++ lua/entities/cw_attpack_sights_longrange/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "cw_attpack_base"
+ENT.PrintName = "Sights - Long Range"
+ENT.PackageText = "Long range sights"
+ENT.Category = "CW 2.0 Attachments"
+ENT.Author = "Spy"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true 
+ENT.Cost = 300
+ENT.Model = "models/Items/BoxSRounds.mdl"
+
+ENT.attachments = {"md_acog", "bg_sg1scope", "md_pso1"}
---
+++ lua/entities/cw_attpack_sights_sniper/shared.lua
@@ -0,0 +1,12 @@
+ENT.Type = "anim"
+ENT.Base = "cw_attpack_base"
+ENT.PrintName = "Sights - Sniper"
+ENT.PackageText = "Sniper sights"
+ENT.Category = "CW 2.0 Attachments"
+ENT.Author = "Spy"
+ENT.Spawnable = true
+ENT.AdminSpawnable = true 
+ENT.Cost = 500
+ENT.Model = "models/Items/BoxSRounds.mdl"
+
+ENT.attachments = {"md_nightforce_nxs"}
