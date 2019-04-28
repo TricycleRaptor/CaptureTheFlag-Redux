76a91f9b3ffb8d9577354acad39f6df7f8442f3a
Arrin Bevers <arrinbevers@yahoo.com>
2019-04-28 13:50:18 -0700

    Balance update
    Nerfed the Scout class and gave each class an additional 50 health (At the exception of Scout)

--- gamemodes/capturetheflag/gamemode/config/custom_classes.lua
+++ gamemodes/capturetheflag/gamemode/config/custom_classes.lua
@@ -1,42 +1,42 @@
 PLAYER_CLASSES = {
 	{
 		name = "Default",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_physcannon"}
 	},
 	{
 		name = "Rifleman",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_crowbar", "cw_ar15", "cw_shorty", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
 	},
 	{
 		name = "Marksman",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_crowbar", "cw_g4p_m98b", "cw_mp5", "cw_fiveseven", "weapon_rpw_binoculars" ,"cw_smoke_grenade"}
 	},
 	{
 		name = "Machinegunner",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_crowbar", "cw_m249_official", "cw_fiveseven", "cw_frag_grenade"}
 	},
 	{
 		name = "Demolitionist",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_crowbar", "weapon_rpg", "weapon_ied", "cw_g4p_m16a4", "weapon_extinguisher"}
+		weapons = {"weapon_crowbar", "weapon_rpg", "weapon_ied", "cw_m3super90", "cw_fiveseven", "weapon_extinguisher"}
 	},
 	{
 		name = "Arsonist",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_crowbar", "cw_ump45", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan", "weapon_extinguisher"}
@@ -46,18 +46,18 @@ PLAYER_CLASSES = {
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_m3super90", "cw_fiveseven", "weapon_simrepair", "weapon_extinguisher"}
+		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_tr09_qbz97", "cw_fiveseven", "weapon_simrepair", "weapon_extinguisher"}
 	},
 	{
 		name = "Scout",
 		health = 100,
-		walkspeed = 300,
-		runspeed = 450,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_g36c", "cw_fiveseven", "weapon_rpw_binoculars_scout", "parkourmod"}
+		walkspeed = 220,
+		runspeed = 320,
+		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_g36c", "cw_fiveseven", "weapon_rpw_binoculars_scout", "climb_swep2"}
 	},
 	{
 		name = "Medic",
-		health = 100,
+		health = 150,
 		walkspeed = 200,
 		runspeed = 300,
 		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_scorpin_evo3", "cw_fiveseven", "weapon_medkit", "cw_smoke_grenade", "cw_smoke_grenade"}
