d1547b4017483f12c5ca14c47ab02b1c0cca6e97
Arrin Bevers <arrinbevers@yahoo.com>
2019-09-07 12:18:16 -0700

    Balance update

--- gamemodes/capturetheflag/gamemode/config/custom_classes.lua
+++ gamemodes/capturetheflag/gamemode/config/custom_classes.lua
@@ -8,58 +8,58 @@ PLAYER_CLASSES = {
 	},
 	{
 		name = "Rifleman",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 250,
-		weapons = {"weapon_crowbar", "cw_g4p_m4a1", "cw_shorty", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
+		runspeed = 280,
+		weapons = {"weapon_crowbar", "cw_g4p_m4a1", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
 	},
 	{
 		name = "Marksman",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 250,
+		runspeed = 280,
 		weapons = {"weapon_crowbar", "cw_m14", "cw_fiveseven", "weapon_rpw_binoculars" ,"cw_smoke_grenade"}
 	},
 	{
 		name = "Machinegunner",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 220,
+		runspeed = 250,
 		weapons = {"weapon_crowbar", "cw_m249_official", "cw_fiveseven", "cw_frag_grenade"}
 	},
 	{
 		name = "Demolitionist",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 220,
-		weapons = {"weapon_crowbar", "weapon_rpg", "weapon_ied", "cw_ump45", "cw_fiveseven", "weapon_extinguisher"}
+		runspeed = 250,
+		weapons = {"weapon_crowbar", "weapon_rpg", "cw_ump45", "cw_fiveseven", "weapon_slam", "weapon_extinguisher"}
 	},
 	{
 		name = "Arsonist",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 250,
-		weapons = {"weapon_crowbar", "cw_m3super90", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan", "weapon_extinguisher"}
+		runspeed = 280,
+		weapons = {"weapon_crowbar", "cw_shorty", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan", "weapon_extinguisher"}
 	},
 	{
 		name = "Engineer",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 250,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_masada_acr", "cw_fiveseven", "weapon_simrepair", "weapon_extinguisher"}
+		runspeed = 280,
+		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_m3super90", "cw_fiveseven", "weapon_simrepair", "weapon_extinguisher"}
 	},
 	{
 		name = "Scout",
-		health = 150,
+		health = 100,
 		walkspeed = 220,
 		runspeed = 280,
 		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_g36c", "cw_fiveseven", "weapon_rpw_binoculars_scout", "climb_swep2"}
 	},
 	{
 		name = "Medic",
-		health = 200,
+		health = 100,
 		walkspeed = 200,
-		runspeed = 250,
+		runspeed = 280,
 		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_mp5", "cw_fiveseven", "weapon_medkit", "cw_smoke_grenade", "cw_smoke_grenade"}
 	}
 }
