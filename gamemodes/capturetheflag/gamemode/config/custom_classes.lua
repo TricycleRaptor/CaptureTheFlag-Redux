10a8c98442b2a2bfde6531dbd9bebb98cdd85de3
Arrin Bevers <arrinbevers@yahoo.com>
2019-09-08 21:06:26 -0700

    TFA Update

--- gamemodes/capturetheflag/gamemode/config/custom_classes.lua
+++ gamemodes/capturetheflag/gamemode/config/custom_classes.lua
@@ -11,55 +11,55 @@ PLAYER_CLASSES = {
 		health = 100,
 		walkspeed = 200,
 		runspeed = 280,
-		weapons = {"weapon_crowbar", "cw_g4p_m4a1", "cw_fiveseven", "cw_frag_grenade", "cw_smoke_grenade"}
+		weapons = {"weapon_crowbar", "tfa_ins2_m4a1", "tfa_ins2_m9", "tfa_ins2_m67"}
 	},
 	{
 		name = "Marksman",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 280,
-		weapons = {"weapon_crowbar", "cw_m14", "cw_fiveseven", "weapon_rpw_binoculars" ,"cw_smoke_grenade"}
+		weapons = {"weapon_crowbar", "tfa_ins2_mk14ebr", "tfa_ins2_usp_match", "weapon_rpw_binoculars"}
 	},
 	{
 		name = "Machinegunner",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 250,
-		weapons = {"weapon_crowbar", "cw_m249_official", "cw_fiveseven", "cw_frag_grenade"}
+		weapons = {"weapon_crowbar", "tfa_ins2_minimi", "tfa_ins2_cz75", "tfa_ins2_f1"}
 	},
 	{
 		name = "Demolitionist",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 250,
-		weapons = {"weapon_crowbar", "weapon_rpg", "cw_ump45", "cw_fiveseven", "weapon_slam", "weapon_extinguisher"}
+		weapons = {"weapon_crowbar", "weapon_rpg", "tfa_ins2_spas12", "tfa_ins2_cz75", "weapon_slam", "tfa_ins2_f1"}
 	},
 	{
 		name = "Arsonist",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 280,
-		weapons = {"weapon_crowbar", "cw_shorty", "cw_fiveseven", "weapon_nmrih_molotov", "weapon_vfire_gascan", "weapon_extinguisher"}
+		weapons = {"weapon_crowbar", "tfa_ins2_spectre", "tfa_ins2_tt33", "weapon_nmrih_molotov", "weapon_vfire_gascan", "weapon_extinguisher"}
 	},
 	{
 		name = "Engineer",
 		health = 100,
 		walkspeed = 200,
-		runspeed = 280,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_m3super90", "cw_fiveseven", "weapon_simrepair", "weapon_extinguisher"}
+		runspeed = 260,
+		weapons = {"weapon_physcannon", "weapon_crowbar", "tfa_ins2_m590", "tfa_ins2_m9", "weapon_simrepair", "alydus_fortificationbuildertablet", "weapon_extinguisher"}
 	},
 	{
 		name = "Scout",
 		health = 100,
 		walkspeed = 220,
 		runspeed = 280,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_g4p_g36c", "cw_fiveseven", "weapon_rpw_binoculars_scout", "climb_swep2"}
+		weapons = {"weapon_physcannon", "weapon_crowbar", "tfa_ins2_mp5k", "tfa_ins2_usp_match", "weapon_rpw_binoculars_scout", "climb_swep2"}
 	},
 	{
 		name = "Medic",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 280,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "cw_mp5", "cw_fiveseven", "weapon_medkit", "cw_smoke_grenade", "cw_smoke_grenade"}
+		weapons = {"weapon_physcannon", "weapon_crowbar", "tfa_ins2_mp7", "tfa_ins2_m9", "weapon_medkit"}
 	}
 }
