9bdce34227db3394ae6b094c3e79c3a96b712269
Arrin Bevers <arrinbevers@yahoo.com>
2020-02-09 12:40:10 -0800

    Prototype weapon selection system
    Added dependency files

---
+++ gamemodes/capturetheflag/gamemode/config/secondary_weapons.lua
@@ -0,0 +1,32 @@
+local V = {
+
+	Name = "QSZ-92",
+	Model = "models/weapons/tfa_ins2/w_qsz92.mdl",
+	Class = "tfa_ins2_qsz92",
+	Category = "Sidearms",
+	Description = "A high caliber pistol with strong damage and recoil.",
+
+}
+list.Set( "ctf_sidearms", "tfa_ins2_qsz92", V )
+
+local V = {
+
+	Name = "Beretta M9",
+	Model = "models/weapons/tfa_ins2/w_m9.mdl",
+	Class = "tfa_ins2_m9",
+	Category = "Sidearms",
+	Description = "Popular self-defense and police sidearm offering moderate damage and low recoil.",
+
+}
+list.Set( "ctf_sidearms", "tfa_ins2_m9", V )
+
+local V = {
+
+	Name = "H&K USP Match",
+	Model = "models/weapons/tfa_ins2/w_usp_match.mdl",
+	Class = "tfa_ins2_usp_match",
+	Category = "Sidearms",
+	Description = "A pistol that exchanges damage for high accuracy and tactical accessories.",
+
+}
+list.Set( "ctf_sidearms", "tfa_ins2_usp_match", V )
