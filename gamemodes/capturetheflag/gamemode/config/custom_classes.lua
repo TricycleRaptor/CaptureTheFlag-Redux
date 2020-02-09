d274b548e26340ef836bbb7cba675fece95c3c41
Arrin Bevers <arrinbevers@yahoo.com>
2020-02-09 13:50:43 -0800

    Removed obsolete weapons arrays

--- gamemodes/capturetheflag/gamemode/config/custom_classes.lua
+++ gamemodes/capturetheflag/gamemode/config/custom_classes.lua
@@ -2,64 +2,56 @@ PLAYER_CLASSES = {
 	{
 		name = "Default",
 		health = 100,
-		walkspeed = 200,
-		runspeed = 250,
-		weapons = {"weapon_physcannon"}
+		walkspeed = 500,
+		runspeed = 550,
+		weapons = "weapon_physcannon"
 	},
 	{
 		name = "Rifleman",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_crowbar", "tfa_ins2_acrc", "tfa_ins2_qsz92", "tfa_ins2_m67"}
 	},
 	{
 		name = "Marksman",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_crowbar", "tfa_ins2_mk14ebr", "tfa_ins2_qsz92", "weapon_rpw_binoculars"}
 	},
 	{
 		name = "Machinegunner",
 		health = 100,
-		walkspeed = 200,
-		runspeed = 280,
-		weapons = {"weapon_crowbar", "tfa_ins2_minimi", "tfa_ins2_qsz92", "tfa_ins2_f1"}
+		walkspeed = 190,
+		runspeed = 270,
 	},
 	{
 		name = "Demolitionist",
 		health = 100,
-		walkspeed = 200,
-		runspeed = 280,
-		weapons = {"weapon_crowbar", "weapon_rpg", "tfa_ins2_mwr_p90", "tfa_ins2_qsz92", "weapon_simmines", "weapon_slam"}
+		walkspeed = 190,
+		runspeed = 270,
 	},
 	{
 		name = "Support",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_crowbar", "tfa_ins2_spas12", "tfa_ins2_qsz92", "tfa_misc_st5stunner", "tfa_fml_hl2_ammo_drop_pistol","tfa_fml_hl2_ammo_drop_smg1","tfa_fml_hl2_ammo_drop_ar2","tfa_fml_hl2_ammo_drop_shotgun"}
 	},
 	{
 		name = "Engineer",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 290,
-		weapons = {"weapon_physcannon", "weapon_crowbar", "tfa_ins2_m590o", "tfa_ins2_qsz92", "weapon_simrepair", "alydus_fortificationbuildertablet", "weapon_extinguisher"}
 	},
 	{
 		name = "Scout",
 		health = 100,
 		walkspeed = 220,
 		runspeed = 320,
-		weapons = {"weapon_crowbar", "tfa_ins2_krissv", "tfa_ins2_qsz92", "weapon_rpw_binoculars_scout", "climb_swep2"}
 	},
 	{
 		name = "Medic",
 		health = 100,
 		walkspeed = 200,
 		runspeed = 300,
-		weapons = {"weapon_crowbar", "tfa_ins2_mp7", "tfa_ins2_qsz92", "tfa_misc_st5stunner" ,"weapon_medkit"}
 	}
 }
