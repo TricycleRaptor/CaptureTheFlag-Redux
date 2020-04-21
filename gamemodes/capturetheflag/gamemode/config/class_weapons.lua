dfca15ce656e934dcc815c124a6f9eb2d59df72a
Arrin Bevers <arrinbevers@yahoo.com>
2020-04-20 19:33:37 -0700

    Security Compatibility Update
    Organized list data and adjusted category labels for new security system.

--- gamemodes/capturetheflag/gamemode/config/class_weapons.lua
+++ gamemodes/capturetheflag/gamemode/config/class_weapons.lua
@@ -5,7 +5,7 @@ local V = {
 	Name = "ARC-C",
 	Model = "models/weapons/w_acrc.mdl",
 	Class = "tfa_ins2_acrc",
-	Category = "Rifleman Primary Weapon",
+	Category = 2,
 	Description = "An assault rifle that offers high damage in bursts while suffering from high recoil and spread.",
 
 }
@@ -16,7 +16,7 @@ local V = {
 	Name = "SCAR-L",
 	Model = "models/weapons/tfa_ins2/w_scarl.mdl",
 	Class = "tfa_ins2_scarl",
-	Category = "Rifleman Primary Weapon",
+	Category = 2,
 	Description = "An assault rifle that offers great recoil control and low spread, moderate damage at medium to long distances.",
 
 }
@@ -29,7 +29,7 @@ local V = {
 	Name = "MK. 14 EBR",
 	Model = "models/weapons/tfa_ins2/w_m14ebr.mdl",
 	Class = "tfa_ins2_mk14ebr",
-	Category = "Marksman Primary Weapon",
+	Category = 3,
 	Description = "Dedicated marksman rifle, offers both automatic and semi-automatic firing modes. Ineffective at long range.",
 
 }
@@ -40,7 +40,7 @@ local V = {
 	Name = "GOL Magnum",
 	Model = "models/weapons/tfa_ins2/w_gol.mdl",
 	Class = "tfa_ins2_gol",
-	Category = "Marksman Primary Weapon",
+	Category = 3,
 	Description = "Long rifle offering high damage, high penetration ammunition. Bolt-action.",
 
 }
@@ -53,7 +53,7 @@ local V = {
 	Name = "RPK-74m",
 	Model = "models/weapons/w_rpk_74m.mdl",
 	Class = "tfa_ins2_rpk_74m",
-	Category = "Gunner Primary Weapon",
+	Category = 4,
 	Description = "Drum-fed machinegun offering moderate firerate and recoil. Decent range.",
 
 }
@@ -64,11 +64,35 @@ local V = {
 	Name = "M60",
 	Model = "models/weapons/w_nam_m60.mdl",
 	Class = "tfa_nam_m60",
-	Category = "Gunner Primary Weapon",
+	Category = 4,
 	Description = "Belt-fed, Vietnam-era machinegun offering high firerate with poor recoil control and moderate range.",
 
 }
 list.Set( "weapons_gunner_primary", "tfa_nam_m60", V )
+
+-- Demolitionist Weapon Table
+
+local V = {
+
+	Name = "QBZ-97",
+	Model = "models/weapons/smc/qbz97/w_warface_t97.mdl",
+	Class = "tfa_ins2_norinco_qbz97",
+	Category = 5,
+	Description = "A bullpup rifle sporting a high capacity magazine and low recoil.",
+
+}
+list.Set( "weapons_demolitionist_primary", "tfa_ins2_mwr_p90", V)
+
+local V = {
+
+	Name = "H&K MP7",
+	Model = "models/weapons/tfa_ins2/w_mp7.mdl",
+	Class = "tfa_ins2_mp7",
+	Category = 5,
+	Description = "A high-capacity SMG with moderate damage and spread, low recoil.",
+
+}
+list.Set( "weapons_demolitionist_primary", "tfa_ins2_mp7", V )
 
 -- Support Weapon Table
 
@@ -77,7 +101,7 @@ local V = {
 	Name = "SPAS-12",
 	Model = "models/weapons/tfa_ins2/w_spas12_bri.mdl",
 	Class = "tfa_ins2_spas12",
-	Category = "Support Primary Weapon",
+	Category = 6,
 	Description = "Combat pump shotgun offering nine shells per full-capacity with tight pellet spread. Effective at close and midrange.",
 
 }
@@ -88,7 +112,7 @@ local V = {
 	Name = "M1014",
 	Model = "models/weapons/tfa_ins2/w_m1014.mdl",
 	Class = "tfa_ins2_m1014",
-	Category = "Support Primary Weapon",
+	Category = 6,
 	Description = "Semi-automatic military shotgun with quick burst potential and medium pellet spread. Best at close range. Holds seven shells.",
 
 }
@@ -101,7 +125,7 @@ local V = {
 	Name = "Nova",
 	Model = "models/weapons/tfa_ins2/w_nova.mdl",
 	Class = "tfa_ins2_nova",
-	Category = "Engineer Primary Weapon",
+	Category = 7,
 	Description = "A tactical pump shotgun offering eight shells with standard pellet count. Tighter spread, medium range.",
 
 }
@@ -112,7 +136,7 @@ local V = {
 	Name = "M590A1",
 	Model = "models/weapons/tfa_ins2/w_m590_olli.mdl",
 	Class = "tfa_ins2_m590o",
-	Category = "Engineer Primary Weapon",
+	Category = 7,
 	Description = "A combat pump shotgun offering nine shells with higher pellet count. Short range.",
 
 }
@@ -125,7 +149,7 @@ local V = {
 	Name = "H&K MP5K",
 	Model = "models/weapons/tfa_ins2/w_mp5k.mdl",
 	Class = "tfa_ins2_mp5k",
-	Category = "Scout Primary Weapon",
+	Category = 8,
 	Description = "A well-rounded SMG with low damage and spread, moderate recoil.",
 
 }
@@ -136,7 +160,7 @@ local V = {
 	Name = "KRISS VECTOR",
 	Model = "models/weapons/tfa_ins2/w_krissv.mdl",
 	Class = "tfa_ins2_krissv",
-	Category = "Scout Primary Weapon",
+	Category = 8,
 	Description = "A standard-capacity SMG with high firerate and spread.",
 
 }
@@ -149,7 +173,7 @@ local V = {
 	Name = "H&K MP5K",
 	Model = "models/weapons/tfa_ins2/w_mp5k.mdl",
 	Class = "tfa_ins2_mp5k",
-	Category = "Medic Primary Weapon",
+	Category = 9,
 	Description = "A well-rounded SMG with low damage and spread, moderate recoil.",
 
 }
@@ -160,33 +184,9 @@ local V = {
 	Name = "H&K MP7",
 	Model = "models/weapons/tfa_ins2/w_mp7.mdl",
 	Class = "tfa_ins2_mp7",
-	Category = "Medic Primary Weapon",
+	Category = 9,
 	Description = "A high-capacity SMG with moderate damage and spread, low recoil.",
 
 }
 list.Set( "weapons_medic_primary", "tfa_ins2_mp7", V )
-
--- Demolitionist Weapon Table
-
-local V = {
-
-	Name = "QBZ-97",
-	Model = "models/weapons/smc/qbz97/w_warface_t97.mdl",
-	Class = "tfa_ins2_norinco_qbz97",
-	Category = "Demolitionist Primary Weapon",
-	Description = "A bullpup rifle sporting a high capacity magazine and low recoil.",
-
-}
-list.Set( "weapons_demolitionist_primary", "tfa_ins2_mwr_p90", V)
-
-local V = {
-
-	Name = "H&K MP7",
-	Model = "models/weapons/tfa_ins2/w_mp7.mdl",
-	Class = "tfa_ins2_mp7",
-	Category = "Demolitionist Primary Weapon",
-	Description = "A high-capacity SMG with moderate damage and spread, low recoil.",
-
-}
-list.Set( "weapons_demolitionist_primary", "tfa_ins2_mp7", V )
 
